b0ae73ef0ddc1c18e2df5da9fd11eab1138491cb
Rokorios <charles.cox@rokorium.com>
2023-06-11 11:36:38 -0500

    0.1
    Added Psudocode and Template

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odin_calculator-simple
   Add, Subtract, Divide, Multiple
+0.1 Create Psudocode
+0.2 Create HTML Skeleton
+0.3 Style Skeleton
+0.4 Add JS Functions
+0.5 Links JS Functions
+0.6 Test and Repair any Discovered Bugs
---
+++ resources/js/script.js
@@ -0,0 +1,4 @@
+//Break input into 3 Variables by spaces {First number} {Operator} {Second Number}
+//when clicking on a number, it adds it to the input screen. When clicking on an operator it adds a space in front and after
+//perform operations
+//calculate - Triggers breaking the input and performing operations, then displays result.
